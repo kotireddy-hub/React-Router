8d667a16bdb7c0e4f158c8f1607660b2a55b9a1d
kotireddy-hub <52464386+kotireddy-hub@users.noreply.github.com>
2023-01-06 20:10:11 +0530

    added product page

--- src/Header/Menu.js
+++ src/Header/Menu.js
@@ -15,8 +15,8 @@ class Menu extends React.Component {
                     <li className="ui_li"><a href="#/home">Home</a></li>
                     <li className="ui_li"><a href="#/about">About</a></li>
                     <li className="ui_li"><NavLink to="service">Service</NavLink></li>
-                    <li className="ui_li"><Link to="contact">Contact</Link></li>
-                    <li className="ui_li"><Link to="contact">Products</Link></li>
+                    <li className="ui_li"><Link to="/contact">Contact</Link></li>
+                    <li className="ui_li"><Link to="/Products">Products</Link></li>
                 </ul>
                 <Routes>
                     <Route path="/" exact element={<Home />} />
@@ -24,6 +24,7 @@ class Menu extends React.Component {
                     <Route path="/about" element={<About />} />
                     <Route path="/service" element={<Service />} />
                     <Route path="/contact" element={<Contact />} />
+                    <Route path="/Products" element={<Contact />} />
                 </Routes>
             </BrowserRouter>
         </div>
